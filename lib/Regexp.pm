999da6622e8ad3b6ad21f03636c1a75c652ba2db
Сединкин Сергей <serg3452@yandex.ru>
2019-08-23 23:31:58 +0400

    Добавляем метод Regexp::delete_words_by_regexp, чиним тест

--- lib/Regexp.pm
+++ lib/Regexp.pm
@@ -1,4 +1,4 @@
-package Brackets;
+package Regexp;
 
 =head1 DESCRIPTION
 
@@ -6,7 +6,30 @@ package Brackets;
 
 =cut
 
+use lib::abs '../lib';
+
 use strict;
 use warnings;
+
+use StringChecker;
+
+=head2 delete_words_by_regexp
+
+Функция, которая удалит одним регекспом из предложения, все слова содержащие повторяющиеся символы.
+
+IN:
+    $string - строка
+
+OUT:
+    die         - $string - не строка, которую ожидаем на входе
+    $new_string - новая строка
+
+=cut
+
+sub delete_words_by_regexp {
+    my ( $string ) = @_;
+
+    StringChecker::validate_data( 0, 'brackets', $string );
+}
 
 1;
