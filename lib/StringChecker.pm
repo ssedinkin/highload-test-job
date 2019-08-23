7c1bf5fbafad748cb6a231982344860c5e22f6a1
Сединкин Сергей <serg3452@yandex.ru>
2019-08-23 20:56:20 +0400

    Добавляем метод StringChecker::_is_not_empty_string

--- lib/StringChecker.pm
+++ lib/StringChecker.pm
@@ -34,6 +34,31 @@ sub _is_string {
     return 1;
 }
 
+=head2 _is_not_empty_string
+
+Проверить входящие данные. Это непустая?
+Пустая строка - не строка.
+Непустая строка - строка.
+Массивы, хэши, undef и прочая фигня - не строка
+
+IN:
+    $data - входящие данные
+
+OUT:
+    0 - не строка
+    1 - строка
+
+=cut
+
+sub _is_not_empty_string {
+    my ( $data ) = @_;
+
+    return 0  unless _is_string( $data );
+    return 0  unless length $data;
+
+    return 1;
+}
+
 =head2 _validate_data_or_die
 
 Валидация входных данных. На входе мы ждем строку, которая состоит из символов:
