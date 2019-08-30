0882395ff9e9acbc3e6f2afe807984d7ca62056f
Сединкин Сергей <serg3452@yandex.ru>
2019-08-30 20:37:53 +0400

    Проверяем входные данные в BinarySearch::_check_data

--- lib/BinarySearch.pm
+++ lib/BinarySearch.pm
@@ -1,4 +1,4 @@
-package StringChecker;
+package BinarySearch;
 
 =head1 DESCRIPTION
 
@@ -8,5 +8,25 @@ package StringChecker;
 
 use strict;
 use warnings;
+use Carp qw/ confess /;
+
+=head2 _check_data
+
+Проверить входящие данные. Это массив?
+
+IN:
+    $data - данные, которые проверяем
+
+OUT:
+    die - $data - не массив
+    1   - $data - массив
+
+=cut
+
+sub _check_data {
+    my ( $data ) = @_;
+
+    confess 'array expected'  unless $data && ref $data eq 'ARRAY';
+}
 
 1;
